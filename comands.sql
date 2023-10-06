956b6c12ea381e07f48dc8591907f870c93cb723
Zimbo Sebastião <gloriasebastiao7@gmail.com>
2023-10-06 10:25:33 -0300

    teste

--- comands.sql
+++ comands.sql
@@ -4,4 +4,4 @@ CREATE TABLE `salva-pets-api`.`cachorros` (`id` INT(20) NOT NULL AUTO_INCREMENT 
 
 
 -- Script para criação da tabela gatos
-CREATE TABLE `salva-pets-api`.`gatos` (`id` INT(20) NOT NULL AUTO_INCREMENT , `nome` VARCHAR(50) NOT NULL , `foto` BLOB NOT NULL , `idade` DATE NOT NULL , `sexo` VARCHAR(15) NOT NULL , `raca` VARCHAR(50) NOT NULL , `descricao` TEXT NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
+CREATE TABLE `salva-pets-api`.`gatos` (`id` INT(20) NOT NULL AUTO_INCREMENT , `nome` VARCHAR(50) NOT NULL , `foto` BLOB NOT NULL , `idade` DATE NOT NULL , `sexo` VARCHAR(15) NOT NULL , `raca` VARCHAR(50) NOT NULL , `descricao` TEXT NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB; 
